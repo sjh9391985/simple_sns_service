f35d328f4c3a0550ab8ac1ec32b7a7c6632e60aa
sjh9391985 <sjh9391985@gmail.com>
2024-03-13 09:15:04 +0900

    feat: "유저 레포지터리"

---
+++ src/main/java/com/example/sns/respository/UserEntityRepository.java
@@ -0,0 +1,11 @@
+package com.example.sns.respository;
+
+import com.example.sns.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+}
