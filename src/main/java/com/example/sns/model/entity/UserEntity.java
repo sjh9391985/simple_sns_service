9541a059cb62731bf9bac8238274a506a6bf0e14
sjh9391985 <sjh9391985@gmail.com>
2024-03-14 13:38:14 +0900

    feat: "회원정보 객체 및 테이블"

--- src/main/java/com/example/sns/model/entity/UserEntity.java
+++ src/main/java/com/example/sns/model/entity/UserEntity.java
@@ -1,22 +1,61 @@
 package com.example.sns.model.entity;
 
+import com.example.sns.model.UserRole;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Setter
 @Getter
 @Entity
-@Table
+@Table(name = "\"user\"")
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() where id = ?")
+@Where(clause = "deleted_at is NULL")
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "user_name")
     private String username;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @Column(name = "register_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static UserEntity of(String username, String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userEntity.setPassword(password);
+        return userEntity;
+
+    }
 }
