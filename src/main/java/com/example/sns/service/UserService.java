599e915dae9b509152a619563738d2fe3353cb60
sjh9391985 <sjh9391985@gmail.com>
2024-03-14 13:38:59 +0900

    chore: "회원가입 서비스"

--- src/main/java/com/example/sns/service/UserService.java
+++ src/main/java/com/example/sns/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.sns.service;
 
+import com.example.sns.exception.ErrorCode;
 import com.example.sns.exception.SnsApplicationException;
 import com.example.sns.model.User;
 import com.example.sns.model.entity.UserEntity;
@@ -18,20 +19,22 @@ public class UserService {
 
     public User join(String username, String password) {
         // 1. 이미 가입된 회원 찾기
-        Optional<UserEntity> userEntity = userEntityRepository.findByUsername(username);
+        userEntityRepository.findByUsername(username).ifPresent(it -> {
+            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", username));
+        });
 
         // 2. 회원가입
-        userEntityRepository.save(new UserEntity());
+        UserEntity user = userEntityRepository.save(UserEntity.of(username, password));
 
-        return new User();
+        return User.fromEntity(user);
     }
 
     public String login(String username, String password) {
         // 1. 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUsername(username).orElseThrow(() -> new SnsApplicationException());
+        UserEntity userEntity = userEntityRepository.findByUsername(username).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,""));
 
         // 2. 비밀번호 체크
-        if(userEntity.getPassword().equals(password)) throw new SnsApplicationException();
+        if (userEntity.getPassword().equals(password)) throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,"");
 
         // 3. 토큰 생성 및 반환
         return "";
