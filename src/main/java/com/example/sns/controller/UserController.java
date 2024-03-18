d9a219ea259e0d1190a465dfcf5a1b464253b103
sjh9391985 <sjh9391985@gmail.com>
2024-03-19 08:53:29 +0900

    feat: "회원 컨트롤러"
    - 로그인
    - 회원가입

---
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.sns.controller;
+
+import com.example.sns.controller.request.UserJoinRequest;
+import com.example.sns.controller.request.UserLoginRequest;
+import com.example.sns.controller.response.Response;
+import com.example.sns.controller.response.UserJoinResponse;
+import com.example.sns.controller.response.UserLoginResponse;
+import com.example.sns.model.User;
+import com.example.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(
+            @RequestBody UserJoinRequest request
+    ) {
+        User user = userService.join(request.getUsername(), request.getPassword());
+        return Response.success(UserJoinResponse.fromUser(user));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(
+            @RequestBody UserLoginRequest request
+    ) {
+        String token = userService.login(request.getUsername(), request.getPassword());
+        return Response.success(new UserLoginResponse(token));
+    }
+
+}
