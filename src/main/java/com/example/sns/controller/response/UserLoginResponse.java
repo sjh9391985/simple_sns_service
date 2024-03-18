fcb0c02ee5af83bed51b8f3925708cb3dc5ad1a1
sjh9391985 <sjh9391985@gmail.com>
2024-03-19 08:53:51 +0900

    feat: "회원가입 응답 DTO"

---
+++ src/main/java/com/example/sns/controller/response/UserLoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.sns.controller.response;
+
+import com.example.sns.model.User;
+import com.example.sns.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponse {
+
+    private String token;
+
+}
