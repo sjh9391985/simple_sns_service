f270b2eb7bd0f05927f5411a6ccc182b42efb6c0
sjh9391985 <sjh9391985@gmail.com>
2024-03-19 08:55:09 +0900

    feat: "예외처리 응답 코드"

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),;
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"user not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "password is invalid"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
 
     private HttpStatus status;
     private String message;
