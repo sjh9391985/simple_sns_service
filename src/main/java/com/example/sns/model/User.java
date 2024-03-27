e20fc03437f4edd9b98f7be7de4f2b565d838e21
sjh9391985 <sjh9391985@gmail.com>
2024-03-27 19:14:00 +0900

    feat: "유저 디테일 인터페이스 구현"

--- src/main/java/com/example/sns/model/User.java
+++ src/main/java/com/example/sns/model/User.java
@@ -3,12 +3,18 @@ package com.example.sns.model;
 import com.example.sns.model.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     private Integer id;
     private String username;
@@ -33,4 +39,36 @@ public class User {
         );
     }
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.getUserRole().toString()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.deletedAt == null;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.deletedAt == null;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.deletedAt == null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.deletedAt == null;
+    }
 }
