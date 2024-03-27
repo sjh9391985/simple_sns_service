429c7768a055de2d6ccbeb2bb5f53ab3c170690c
sjh9391985 <sjh9391985@gmail.com>
2024-03-27 19:13:13 +0900

    chore: "로그인 반환 응답 값 일부 수정"

--- src/main/java/com/example/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/example/sns/configuration/AuthenticationConfig.java
@@ -1,6 +1,10 @@
 package com.example.sns.configuration;
 
 import com.example.sns.configuration.filter.JwtTokenFilter;
+import com.example.sns.exception.CustomAuthenticationEntryPoint;
+import com.example.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,23 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserService userService;
+
+    @Value("${jwt.secret.key}")
+    private String key;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
-                .antMatchers("/api/**").authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//                 Todo
-//                .exceptionHandling()
-//                .authenticationEntryPoint()
+        http.csrf().disable().authorizeRequests().antMatchers("/api/*/users/join", "/api/*/users/login").permitAll().antMatchers("/api/**").authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
     }
 }
