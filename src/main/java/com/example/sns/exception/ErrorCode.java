033fa5376c350366cd49a4dc108816209d1dbe33
sjh9391985 <sjh9391985@gmail.com>
2024-03-27 19:13:32 +0900

    feat: "유효성 없는 토큰 에러 코드"

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"user not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "password is invalid"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid");
 
     private HttpStatus status;
     private String message;
