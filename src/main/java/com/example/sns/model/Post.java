b58e9ebe6c4d5affde1d11d3fbfe81dd51f1ab15
sjh9391985 <sjh9391985@gmail.com>
2024-04-01 19:03:46 +0900

    feat: "게시글 모델"

---
+++ src/main/java/com/example/sns/model/Post.java
@@ -0,0 +1,40 @@
+package com.example.sns.model;
+
+import com.example.sns.model.entity.PostEntity;
+import com.example.sns.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class Post{
+
+    private Integer id;
+    private String title;
+    private String body;
+    private User user;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public static Post fromEntity(PostEntity entity) {
+        return new Post(
+                entity.getId(),
+                entity.getTitle(),
+                entity.getBody(),
+                User.fromEntity(entity.getUser()),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getDeletedAt()
+        );
+    }
+
+}
