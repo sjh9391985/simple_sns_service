52651c6cd5cb6adde51f3e55eef6664c0378e86b
sjh9391985 <sjh9391985@gmail.com>
2024-04-19 22:24:14 +0900

    chore: "삭제 기능 쿼리 최적화"

--- src/main/java/com/example/sns/respository/CommentEntityRepository.java
+++ src/main/java/com/example/sns/respository/CommentEntityRepository.java
@@ -5,7 +5,15 @@ import com.example.sns.model.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CommentEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(PostEntity postEntity);
 }
--- src/main/java/com/example/sns/respository/LikeEntityRepository.java
+++ src/main/java/com/example/sns/respository/LikeEntityRepository.java
@@ -4,7 +4,9 @@ import com.example.sns.model.entity.LikeEntity;
 import com.example.sns.model.entity.PostEntity;
 import com.example.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +19,9 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 
     long countByPost(PostEntity post);
     List<LikeEntity> findAllByPost(PostEntity postEntity);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE LikeEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(PostEntity postEntity);
 }
